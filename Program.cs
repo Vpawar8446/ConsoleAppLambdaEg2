f41ffe0a0816ecaa04f508ab1a89ffb57b6d4a5b
niles <niles@LAPTOP-3K24CQ1R>
2021-09-08 21:35:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleAppLambdaEg2
+{
+    class Student
+    {  
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student {Name ="John", Age=24},
+                new Student {Name="Smith", Age=20},
+                new Student {Name="Sandy", Age=23}
+            };
+
+            var names = students.Select(x => x.Name);
+            foreach(var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.Read();
+
+            /*var age = students.Select(x => x.Age);
+            foreach (var ages in age)
+            {
+                Console.WriteLine(ages);
+            }
+            Console.Read();*/
+        }
+    }
+}
